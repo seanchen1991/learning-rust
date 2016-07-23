b1b119754fb12d717f65bb252e032536730e8842
Sean Chen <skypemaster007@gmail.com>
2016-07-23 13:34:00 -0700

    Write some tests

--- lists/src/first.rs
+++ lists/src/first.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 pub struct List {
     head: Link,
 }
@@ -17,10 +19,56 @@ impl List {
         List { head: Link::Empty }
     }
 
-    pub fn push(self, elem: i32) {
-        let new_node = Node {
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
             elem: elem,
-            next: self.head,
-        };
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(boxed_node) => {
+                let node = *boxed_node;
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    fn basics() {
+        let mut list = List::new();
+
+        // Check the empty list behaves correctly
+        assert_eq!(list.pop(), None);
+
+        // Populate the list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more stuff onto our List
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
     }
 }
